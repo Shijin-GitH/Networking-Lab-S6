2e00f35d524383e1a40173c2a6a6e3c6a2204ce3
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2025-04-07 13:05:04 +0530

    Updated Link State Routing

--- Link State Routing/code.c
+++ Link State Routing/code.c
@@ -1,74 +1,58 @@
 #include <stdio.h>
-#define INF 99999
+#include <limits.h>
 
 void main() {
-    typedef struct {
-        int node1, node2, cost;
-    } Link;
-
-    typedef struct {
-        int weight, parent;
-    } Node;
-
-    int numNodes, numLinks, sourceNode, currentNode;
+    int numNodes, numLinks, sourceNode;
     printf("Enter number of nodes: ");
     scanf("%d", &numNodes);
     printf("Enter number of links: ");
     scanf("%d", &numLinks);
-    Link links[numLinks + 1];
-    Node nodes[numNodes + 1];
-    int visited[numNodes + 1];
-    for (int i = 1; i <= numNodes; i++) {
+    int matrix[numNodes + 1][numNodes + 1], dist[numNodes + 1], visited[numNodes + 1];
+
+    for (int i = 0; i <= numNodes; i++) {
+        for (int j = 0; j <= numNodes; j++) {
+            matrix[i][j] = 0; // Initialize adjacency matrix to 0
+        }
+        dist[i] = INT_MAX; // Initialize distances
         visited[i] = 0; // Initialize visited array to 0
     }
 
-    for (int i = 1; i <= numLinks; i++) {
-        printf("Enter link %d (node1 node2 cost): ", i);
-        scanf("%d %d %d", &links[i].node1, &links[i].node2, &links[i].cost);
+    printf("Enter the links (node1 node2 cost):\n");
+    for (int i = 0; i < numLinks; i++) {
+        int node1, node2, cost;
+        scanf("%d %d %d", &node1, &node2, &cost);
+        matrix[node1][node2] = cost;
+        matrix[node2][node1] = cost; // Assuming undirected graph
     }
-    for (int i = 1; i <= numNodes; i++) {
-        nodes[i].weight = INF; // Initialize weights to infinity
-        nodes[i].parent = -1;   // Initialize parents to -1
-    }
+
     printf("Enter source node: ");
     scanf("%d", &sourceNode);
-    nodes[sourceNode].weight = 0; // Set source node weight to 0
-    nodes[sourceNode].parent = sourceNode; // Set source node parent to itself
+    dist[sourceNode] = 0; // Distance to source node is 0
+    for (int i = 1; i <= numNodes; i++)
+    {
+        int min = INT_MAX, minIndex = -1;
+        for (int j = 1; j <= numNodes; j++) {
+            if (!visited[j] && dist[j] < min) {
+                min = dist[j];
+                minIndex = j;
+            }
+        }
+        if (minIndex == -1) break; // All reachable nodes are visited
+        visited[minIndex] = 1; // Mark the node as visited
 
-    visited[sourceNode] = 1; // Mark source node as visited
-    currentNode = sourceNode;
-    while (1) {
-        int minWeight = INF;
-        int nextNode = -1;
-        for (int i = 1; i < numLinks; i++) {
-            if (links[i].node1 == currentNode && !visited[links[i].node2]) {
-                if (nodes[links[i].node2].weight > nodes[currentNode].weight + links[i].cost) {
-                    nodes[links[i].node2].weight = nodes[currentNode].weight + links[i].cost;
-                    nodes[links[i].node2].parent = currentNode;
-                }
-            } else if (links[i].node2 == currentNode && !visited[links[i].node1]) {
-                if (nodes[links[i].node1].weight > nodes[currentNode].weight + links[i].cost) {
-                    nodes[links[i].node1].weight = nodes[currentNode].weight + links[i].cost;
-                    nodes[links[i].node1].parent = currentNode;
-                }
+        for (int j = 1; j <= numNodes; j++) {
+            if (!visited[j] && matrix[minIndex][j] != 0 && dist[minIndex] != INT_MAX && dist[minIndex] + matrix[minIndex][j] < dist[j]) {
+                dist[j] = dist[minIndex] + matrix[minIndex][j]; // Update distance
             }
         }
-        for (int i = 1; i <= numNodes; i++) {
-            if (!visited[i] && nodes[i].weight < minWeight) {
-                minWeight = nodes[i].weight;
-                nextNode = i;
-            }
-        }
-        if (nextNode == -1) break; // No more reachable nodes
-        visited[nextNode] = 1; // Mark next node as visited
-        currentNode = nextNode; // Move to the next node
     }
-    printf("Shortest paths from source node %d:\n", sourceNode);
+        
+    printf("Shortest distances from node %d:\n", sourceNode);
     for (int i = 1; i <= numNodes; i++) {
-        if (nodes[i].weight != INF) {
-            printf("Node %d: Cost = %d, Parent = %d\n", i, nodes[i].weight, nodes[i].parent);
-        } else {
+        if (dist[i] == INT_MAX) {
             printf("Node %d: Unreachable\n", i);
+        } else {
+            printf("Node %d: %d\n", i, dist[i]);
         }
     }
 }
